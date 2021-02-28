1b9499342a00880bc17a726c48c9dba357ae3d8d
Evan Heckert <evan.heckert@gmail.com>
2021-02-27 18:27:29 -0600

    Updated Dependencies

--- src/caps/DCS/index.js
+++ src/caps/DCS/index.js
@@ -1,11 +1,11 @@
 /* eslint-disable camelcase */
 import { cherryStem, cylindrical } from '../../utils/constants'
-import DCS_R0 from './DCS_R0'
-import DCS_R1 from './DCS_R1'
-import DCS_R2 from './DCS_R2'
-import DCS_R3 from './DCS_R3'
-import DCS_R4 from './DCS_R4'
-import DCS_R5 from './DCS_R5'
+// import DCS_R0 from './DCS_R0'
+// import DCS_R1 from './DCS_R1'
+// import DCS_R2 from './DCS_R2'
+// import DCS_R3 from './DCS_R3'
+// import DCS_R4 from './DCS_R4'
+// import DCS_R5 from './DCS_R5'
 
 const DCS = {
   faceShape: cylindrical,
@@ -13,12 +13,12 @@ const DCS = {
   isSelected: true,
   label: 'DCS',
   manufacturer: 'Signature Plastics',
-  R0: { svg: DCS_R0, depthMax: null, depthMin: null },
-  R1: { svg: DCS_R1, depthMax: null, depthMin: null },
-  R2: { svg: DCS_R2, depthMax: null, depthMin: null },
-  R3: { svg: DCS_R3, depthMax: null, depthMin: null },
-  R4: { svg: DCS_R4, depthMax: null, depthMin: null },
-  R5: { svg: DCS_R5, depthMax: null, depthMin: null },
+  // R0: { svg: DCS_R0, depthMax: null, depthMin: null },
+  // R1: { svg: DCS_R1, depthMax: null, depthMin: null },
+  // R2: { svg: DCS_R2, depthMax: null, depthMin: null },
+  // R3: { svg: DCS_R3, depthMax: null, depthMin: null },
+  // R4: { svg: DCS_R4, depthMax: null, depthMin: null },
+  // R5: { svg: DCS_R5, depthMax: null, depthMin: null },
   sizeX: null,
   sizeY: null,
   sizeZmax: null,
